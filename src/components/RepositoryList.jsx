ac9c47d83d29935e41eefb99c4b6c60366e759f3
Alessandro Marques <vyseclown@gmail.com>
2021-03-10 21:58:31 -0300

    chore: adding the fetch for the repositories from github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,8 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
-import '../styles/repositories.scss'
+import "../styles/repositories.scss";
+
+//https://api.github.com/users/vyseclown/repos
 
 const repository = {
   name: "unform2",
@@ -8,14 +11,22 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/vyseclown/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map((repository) => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}
       </ul>
     </section>
   );
